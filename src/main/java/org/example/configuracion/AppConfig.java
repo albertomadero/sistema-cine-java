24f5d3c11d87b7831b8af3c3c10b46af9bca0456
Alberto Madero <venegasm.alberto@gmail.com>
2026-09-17 21:54:22 -0600

    Agregar api para crear reservas

--- src/main/java/org/example/configuracion/AppConfig.java
+++ src/main/java/org/example/configuracion/AppConfig.java
@@ -1,13 +1,11 @@
 package org.example.configuracion;
 
+import org.example.gestores.GestorAsientos;
 import org.example.gestores.GestorFunciones;
-import org.example.modelo.Funcion;
-import org.example.modelo.Pelicula;
-import org.example.modelo.Sala;
-import org.example.repositorios.RepositorioAsientos;
-import org.example.repositorios.RepositorioFuncion;
-import org.example.repositorios.RepositorioPelicula;
-import org.example.repositorios.RepositorioSala;
+import org.example.gestores.GestorReservas;
+import org.example.gestores.GestorUsuarios;
+import org.example.modelo.*;
+import org.example.repositorios.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,5 +36,66 @@ public class AppConfig {
         }
 
         return gestorFunciones;
+    }
+
+    @Bean
+    public GestorUsuarios gestorUsuarios() {
+        // Crear repositorio
+        RepositorioUsuarios repositorioUsuarios = new RepositorioUsuarios();
+
+        // Crear gestor
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+
+        // Leer y crear la lista
+        List<Usuario> usuarios = repositorioUsuarios.leerUsuarios();
+
+        // Cargar usuarios al gestorUsuarios
+        for (Usuario usuario : usuarios) {
+            gestorUsuarios.cargarUsuarioExistente(usuario);
+        }
+
+        return gestorUsuarios;
+
+    }
+
+    @Bean
+    public GestorAsientos gestorAsientos() {
+        // Crear repo
+        RepositorioAsientos repositorioAsientos = new RepositorioAsientos();
+
+        // Leer y crear lista
+        List<Asiento> asientos = repositorioAsientos.leerAsientos();
+
+        // Crear gestor
+       GestorAsientos gestorAsientos = new GestorAsientos();
+
+        // Cargar asientos al gestorAsientos
+        for (Asiento asiento : asientos) {
+            gestorAsientos.cargarAsientoExistente(asiento);
+        }
+
+        return gestorAsientos;
+    }
+
+    @Bean
+    public GestorReservas gestorReservas(GestorUsuarios gestorUsuarios, GestorAsientos gestorAsientos, GestorFunciones gestorFunciones) {
+        // Crear repo
+        RepositorioReservas repositorioReservas = new RepositorioReservas();
+
+        // Leer y crear listas
+        List<Usuario> usuarios = gestorUsuarios.getUsuarios();
+        List<Asiento> asientos = gestorAsientos.getAsientos();
+        List<Funcion> funciones = gestorFunciones.getListaFunciones();
+        List<Reserva> reservas = repositorioReservas.leerReservas(usuarios, asientos, funciones);
+
+        // Crear gestor
+        GestorReservas gestorReservas = new GestorReservas();
+
+        // Cargar reservas al gestor
+        for (Reserva reserva : reservas) {
+            gestorReservas.cargarReservaExistente(reserva);
+        }
+
+        return gestorReservas;
     }
 }
