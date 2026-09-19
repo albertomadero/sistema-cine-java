26483b8937c1bea4613cc9e3ef6b5106472449b3
Alberto Madero <venegasm.alberto@gmail.com>
2026-09-18 21:01:35 -0600

    Agrega api para buscar asientos disponibles de una funcion

--- src/main/java/org/example/controladores/FuncionController.java
+++ src/main/java/org/example/controladores/FuncionController.java
@@ -1,9 +1,15 @@
 package org.example.controladores;
 
 import org.example.gestores.GestorFunciones;
+import org.example.gestores.GestorReservas;
+import org.example.modelo.Asiento;
 import org.example.modelo.Funcion;
+import org.example.utilidades.BuscadorUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,13 +17,25 @@ import java.util.List;
 public class FuncionController {
 
     private final GestorFunciones gestorFunciones;
+    private final GestorReservas gestorReservas;
 
-    public FuncionController(GestorFunciones gestorFunciones) {
+    public FuncionController(GestorFunciones gestorFunciones, GestorReservas gestorReservas) {
         this.gestorFunciones = gestorFunciones;
+        this.gestorReservas = gestorReservas;
     }
 
     @GetMapping("/funciones")
     public List<Funcion> verFunciones() {
         return gestorFunciones.getListaFunciones();
+    }
+
+    @GetMapping("/funciones/{id}/asientos")
+    public List<Asiento> verAsientosDisponibles(@PathVariable int id) {
+        Funcion funcionBuscada = BuscadorUtil.buscarPorId(id, gestorFunciones.getListaFunciones());
+
+        if (funcionBuscada != null) {
+            return gestorReservas.asientosDisponibles(funcionBuscada);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
