a395e94a839bd4090bc9eacae9a4ebc8877b3a18
Alberto Madero <venegasm.alberto@gmail.com>
2026-09-18 21:36:34 -0600

    Agrega persistencia de datos en api crear-reserva y agrega bean de repostorio reservas

--- src/main/java/org/example/controladores/ReservaController.java
+++ src/main/java/org/example/controladores/ReservaController.java
@@ -10,6 +10,7 @@ import org.example.modelo.Asiento;
 import org.example.modelo.Funcion;
 import org.example.modelo.Reserva;
 import org.example.modelo.Usuario;
+import org.example.repositorios.RepositorioReservas;
 import org.example.utilidades.BuscadorUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class ReservaController {
@@ -26,12 +28,16 @@ public class ReservaController {
     private final GestorAsientos gestorAsientos;
     private final GestorFunciones gestorFunciones;
     private final GestorReservas gestorReservas;
+    private final RepositorioReservas repositorioReservas;
 
-    public ReservaController(GestorUsuarios gestorUsuarios, GestorAsientos gestorAsientos, GestorFunciones gestorFunciones, GestorReservas gestorReservas) {
+    public ReservaController(GestorUsuarios gestorUsuarios, GestorAsientos gestorAsientos,
+                             GestorFunciones gestorFunciones, GestorReservas gestorReservas,
+                             RepositorioReservas repositorioReservas) {
         this.gestorUsuarios = gestorUsuarios;
         this.gestorAsientos = gestorAsientos;
         this.gestorFunciones = gestorFunciones;
         this.gestorReservas = gestorReservas;
+        this.repositorioReservas = repositorioReservas;
     }
 
     @PostMapping("/reservas")
@@ -39,11 +45,15 @@ public class ReservaController {
         Usuario usuarioBuscado = BuscadorUtil.buscarPorId(request.getIdUsuario(), gestorUsuarios.getUsuarios());
         Asiento asientoBuscado = BuscadorUtil.buscarPorId(request.getIdAsiento(), gestorAsientos.getAsientos());
         Funcion funcionBuscada = BuscadorUtil.buscarPorId(request.getIdFuncion(), gestorFunciones.getListaFunciones());
+        Reserva reservaNueva = null;
 
         // Crear reserva
         int idReserva = gestorReservas.generarNuevoId();
         try {
-            return gestorReservas.crearReserva(usuarioBuscado, asientoBuscado, funcionBuscada, LocalDateTime.now(), idReserva);
+            reservaNueva = gestorReservas.crearReserva(usuarioBuscado, asientoBuscado, funcionBuscada, LocalDateTime.now(), idReserva);
+            List<Reserva> listaReservas = gestorReservas.getReservas();
+            repositorioReservas.guardarReserva(listaReservas);
+            return reservaNueva;
         } catch (AsientoYaReservadoException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
